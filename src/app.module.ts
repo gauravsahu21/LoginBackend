dc65a2175b2d5ebef62315ac2a9cb4303abfddf4
Gaurav Kumar <gk989kumar@gmail.com>
2023-09-11 11:36:05 +0530

    contactus routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module} from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { KmpDatabase } from './connections/mysql/kmp.module';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
   imports: [ApiModule, KmpDatabase, 
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 
 
---
+++ src/db/entity/contactus.entity.ts
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import { Entity, Column, BaseEntity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: 'contactus' })
+export class ContactUsEntity extends BaseEntity {
+
+    @PrimaryColumn({ type: 'varchar', length: 255 })
+    contactUsId: string;
+
+    @Column({ type: 'varchar', length: 10 })
+    contactUsType: string;
+
+    @Column({ type: 'text' })
+    message: string;
+
+    @Column({ type: 'varchar', length: 5 })
+    countryCode: string;
+
+    @Column({ type: 'varchar', length: 11 })
+    contactNumber: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    connectorName: string;
+
+    @Column({ type: 'json', nullable: true })
+    contactSubject: any;
+
+    @Column({ type: 'datetime' })
+    contactTime: Date;
+
+    @Column({ type: 'tinyint' })
+    connectStatus: number;
+
+    @Column({ type: 'text', nullable: true })
+    additionalNotes: string;
+}
