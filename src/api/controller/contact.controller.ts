f56784b14475bf8b95a7c91278bed72035ee3db6
Gaurav Kumar <gk989kumar@gmail.com>
2023-09-11 12:18:34 +0530

    deployment configuration

--- src/api/controller/contact.controller.ts
+++ src/api/controller/contact.controller.ts
@@ -3,6 +3,7 @@
 import { Controller, Post, UseGuards, Body, Get, Delete, Param} from "@nestjs/common";
 import { contactUsDto, enquiriesDto, updateEnquiryDto } from "src/common/dto/contact.dto";
 import { ContactUsService } from "../service/contact.service";
+import { WriteAccess } from "../jwt-auth.guard";
 
 @Controller('contactus')
 export class ContactUsController {
@@ -14,22 +15,26 @@ export class ContactUsController {
     }
 
     @Get('newsubmissions')
+    @UseGuards(WriteAccess)
     getContactUs() {
         return this.messagesService.getContactUs();
     }
 
     @Post('getall/submissions')
+    @UseGuards(WriteAccess)
     getAllEnquiries(@Body() enquiry:enquiriesDto) {
        return this.messagesService.getAllEnquiries(enquiry);
     }
    
 
     @Post('update')
+    @UseGuards(WriteAccess)
     updateContactUs(@Body() update: updateEnquiryDto) {
       return this.messagesService.updateContactUs(update);
     }
 
    @Delete('delete/:id')
+   @UseGuards(WriteAccess)
    deleteContactUs(@Param('id') id: string){
     return this.messagesService.deleteContactUs(id)
    }
