f9e405dd2b5aabf43b6d5de3ef3ee05501216d97
suraj-mane <sun.mane1100@gmail.com>
2023-09-29 19:57:45 +0530

    chnage added in routes.ts files

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,52 +1,52 @@
-export default function accessibility  (
+export default function accessibility(
   permissions: any,
   method: string,
   url: string,
-)  {
+) {
   const mappings = {
-    'brands': {
-      GET: permissions['brand']['view'],
-      POST: permissions['brand']['addEdit'],
-      DELETE: permissions['brand']['delete'],
+    brands: {
+      GET: permissions?.brands?.view,
+      POST: permissions?.brands?.addEdit,
+      DELETE: permissions?.brands?.delete,
     },
-    'changepassword':{
-        POST: true,
+    changepassword: {
+      POST: true,
     },
-    'users': {
+    users: {
       GET: permissions['users']['view'],
       POST: permissions['users']['addEdit'],
       DELETE: permissions['users']['delete'],
     },
-    'catalogue': {
-      GET: permissions['catelogues']['view'],
-      POST: permissions['catelogues']['addEdit'],
-      DELETE: permissions['catelogues']['delete'],
+    catalogues: {
+      GET: permissions['catalogues']['view'],
+      POST: permissions['catalogues']['addEdit'],
+      DELETE: permissions['catalogues']['delete'],
     },
-    'careers': {
-      GET: permissions['careers']['view'],
-      POST: permissions['careers']['addEdit'],
-      DELETE: permissions['careers']['delete'],
+    careers: {
+      GET: permissions?.careers?.view,
+      POST: permissions?.careers?.addEdit,
+      DELETE: permissions?.careers?.delete,
     },
-    'certificates': {
-      GET: permissions['certificates']['view'],
-      POST: permissions['certificates']['addEdit'],
-      DELETE: permissions['certificates']['delete'],
+    certificates: {
+      GET: permissions?.certificates?.view,
+      POST: permissions?.certificates?.addEdit,
+      DELETE: permissions?.certificates?.delete,
     },
-    'queries': {
+    queries: {
       GET: permissions['queries']['view'],
       POST: permissions['queries']['addEdit'],
       DELETE: permissions['queries']['delete'],
     },
-    'videos': {
-      GET: permissions['video']['view'],
-      POST: permissions['video']['addEdit'],
-      DELETE: permissions['video']['delete'],
+    videos: {
+      GET: permissions['videos']['view'],
+      POST: permissions['videos']['addEdit'],
+      DELETE: permissions['videos']['delete'],
     },
-    'applicants': {
+    applicants: {
       GET: permissions['applicants']['view'],
       POST: permissions['applicants']['addEdit'],
     },
   };
-  if(!mappings[url]) return "Route not found in metadata";
+  if (!mappings[url]) return 'Route not found in metadata';
   return mappings[url][method];
-};
+}
