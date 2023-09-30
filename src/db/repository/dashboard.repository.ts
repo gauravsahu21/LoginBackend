cbdbd9b29f20da59129a8f40e6c70b2bc6ebb3b1
madhurgera2 <madhurgera2@gmail.com>
2023-09-30 21:34:30 +0530

    helpppp

--- src/db/repository/dashboard.repository.ts
+++ src/db/repository/dashboard.repository.ts
@@ -5,24 +5,32 @@ import { Injectable } from '@nestjs/common';
 export default class DashboardRepository {
   async getDashborad() {
     return {
-      horizontalBarChart: {
-        SMS: [1, 2, 3, 4, 4],
-        call: [2, 3, 4, 5, 6],
-        contactForm: [2, 3, 4, 5, 6],
-      },
       verticalBarChart: {
+        label: ['Jan', 'Feb', 'Mar', 'Apr'],
+        sms: [1, 2, 3, 4],
+        call: [2, 3, 4, 5],
+        contactForm: [2, 3, 4, 5],
+      },
+      horizontalBarChart: {
         brands: 50,
         catelogues: 60,
         videos: 70,
         certificates: 90,
-        aboutUs: 90,
+        aboutus: 90,
         home: 89,
       },
-      queryStatus: {
-        addressed: 90,
-        pending: 78,
-      },
+      queryStatus: [90,78],
       heatmap: {},
+      visits:{
+        week:{
+          unique: 100,
+          total: 200
+        },
+        month:{
+          unique: 100,
+          total: 200
+        }
+      }
     };
   }
 }
