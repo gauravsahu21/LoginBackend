cc832c3d11f1b6b3bcad5ad37de2debb3535090b
Gaurav Kumar <gk989kumar@gmail.com>
2023-10-26 20:08:20 +0530

    deploysite

--- src/api/controller/deploy.controller.ts
+++ src/api/controller/deploy.controller.ts
@@ -1,16 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { DeployService } from "../service/deploy.service";
-import { PermissionsAuthGuard } from "../jwt-auth.guard";
+import { WriteAccess } from "../jwt-auth.guard";
 
 @Controller('/deploy')
 export default class DeployController{
     constructor(private readonly deploy:DeployService){}
 
 @Get('')    
-@UseGuards(PermissionsAuthGuard)
+@UseGuards(WriteAccess )
 async saveFile(){
-    console.log("yes!!");
    return this.deploy.saveFile();
 }
 }
