b9b5a2da99dcea18ad7aa40a0af55ba40dd2e981
Gaurav Kumar <gk989kumar@gmail.com>
2023-11-06 19:05:46 +0530

    applyJob

--- src/db/repository/applicants.repository.ts
+++ src/db/repository/applicants.repository.ts
@@ -9,7 +9,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { CareerEntity } from '../entity/careers.entity';
 import { CareerDto } from 'src/common/dto/careers.dto';
 import { Applicant } from '../entity/applicants.entity';
-import { EditApplicants } from 'src/common/dto/applicants.dto';
+import { ApplyJobDto, EditApplicants } from 'src/common/dto/applicants.dto';
 import logger from '../../connections/logger/logger';
 
 @Injectable()
@@ -44,6 +44,27 @@ export default class ApplicantsRepository {
       return true;
     } catch (error) {
       logger.info("Error occured in editApplicantStatus.Repo")
+      logger.error(error)
+      throw new HttpException('Something went wrong!', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async applyJob(applicantDetails:ApplyJobDto){
+    try{
+      const newJob=new Applicant();
+       newJob.applicantId= uuidv4();
+       newJob.applicantName=applicantDetails.applicantName;
+       newJob.contact=applicantDetails.contact;
+       newJob.experienceinfo=applicantDetails.experienceinfo;
+       newJob.resumeId=applicantDetails.resumeId;
+       newJob.s3Link=applicantDetails.s3Link;
+       newJob.appliedOn=applicantDetails.appliedOn;
+       newJob.careerId=applicantDetails.careerId;
+       newJob.applicantStatus=applicantDetails.applicantStatus;
+      await newJob.save();
+
+    }catch(error){
+      logger.info("Error occured in applyJob.Repo")
       logger.error(error)
       throw new HttpException('Something went wrong!', HttpStatus.NOT_FOUND);
     }
