e9aa21de3a2d7e9098b7a33ae97991065383c260
Gaurav Kumar <gk989kumar@gmail.com>
2023-11-11 16:55:10 +0530

    website

--- src/db/entity/careers.entity.ts
+++ src/db/entity/careers.entity.ts
@@ -1,52 +1,50 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  BaseEntity,
-} from 'typeorm';
+/* eslint-disable prettier/prettier */
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
 @Entity('careers')
 export class CareerEntity extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    careerId: string;
-  
-    @Column({ type: 'varchar', length: 200 })
-    jobTitle: string;
-  
-    @Column({ type: 'json' })
-    experienceLevel: { min: number; max: number };
+  @PrimaryGeneratedColumn('uuid')
+  careerId: string;
 
-    @Column({type: 'int'})
-    jobstatus: number;
-  
-    @Column({ type: 'int' })
-    workMode: number;
-  
-    @Column({ type: 'json' })
-    location: { city: string; state: string; country: string };
-  
-    @Column({ type: 'smallint' })
-    noOfOpenings: number;
-  
-    @Column({ type: 'varchar', length: 30 })
-    publishDate: Date;
-  
-    @Column({ type: 'varchar', length: 30 })
-    unPublishDate: Date;
-  
-    @Column({ type: 'smallint' })
-    employementType: number;
-  
-    @Column({ type: 'varchar', length: 50 })
-    department: string;
-  
-    @Column({ type: 'json' })
-    description: {
-      company: string
-      responbilities: string;
-      education: string;
-      additionalInfo: string;
-      skills: string[];
-    };
-  
+  @Column({ type: 'varchar', length: 200 })
+  jobTitle: string;
+
+  @Column({ type: 'json' })
+  experienceLevel: { min: number; max: number };
+
+  @Column({ type: 'int' })
+  jobstatus: number;
+
+  @Column({ type: 'int' })
+  workMode: number;
+
+  @Column({ type: 'json' })
+  location: { city: string; state: string; country: string };
+
+  @Column({ type: 'smallint' })
+  noOfOpenings: number;
+
+  @Column({ type: 'varchar', length: 30 })
+  publishDate: Date;
+
+  @Column({ type: 'varchar', length: 30 })
+  unPublishDate: Date;
+
+  @Column({ type: 'smallint' })
+  employementType: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  department: string;
+
+  @Column({ type: 'varchar', length: 30 })
+ createdDate: Date;
+
+  @Column({ type: 'json' })
+  description: {
+    company: string;
+    responbilities: string;
+    education: string;
+    additionalInfo: string;
+    skills: string[];
+  };
 }
